24d02297d09efa175b3a76a213c9a3d39560b168
Ryan Gallaway <rkgallaway@msn.com>
2020-03-05 15:02:49 -0800

    re-add clss 09 with correct quiz

--- class-09/demo/js/app.js
+++ class-09/demo/js/app.js
@@ -1,9 +1,9 @@
-function askName(){
+function askName() {
   var username = prompt('What is your Name?');
   return document.write('<h3>' + 'Nice to meet you, ' + username + '</h3>');
 }
 
-function offerGreeting(){
+function offerGreeting() {
   var today = new Date();
   var hourNow = today.getHours();
 
@@ -17,43 +17,43 @@ function offerGreeting(){
   } else {
     greeting = 'Welcome!';
   }
-  message = '<h3>'+ greeting + '</h3>'
+  message = '<h3>' + greeting + '</h3>'
   return document.write(message);
 }
 
-function getItem(){
+function getItem() {
   var preference = prompt('Would you like house or hotel?');
   var item;
   // while true and true // this keeps running / ONE FALSE EJECTS FROM WHILE LOOP
-  while(preference !== 'house' && preference !== 'hotel'){
+  while (preference !== 'house' && preference !== 'hotel') {
     preference = prompt('Would you like house or hotel? PLEASE ANSWER CORRECTLY');
   }
   if (preference === 'house') {
     item = '<img src="images/house.png">';
   } else if (preference === 'hotel') {
     item = '<img src="images/hotel.png">';
-  } 
+  }
   return item;
 }
 
-function howMany(){
+function howMany() {
   var count = prompt('Sounds good, how many would you like?');
   // while true and true it keeps asking for a number
-  while (count === '' || isNaN(count)){
+  while (count === '' || isNaN(count)) {
     var count = prompt('How Many would you like to order?  ENTER A NUMBER!');
   }
   return count;
 }
 
-function showOrder(){
+function showOrder() {
   var item = getItem();
   var total = howMany();
   result = '';
 
   // adds a paragraph with an image in it for each number of times
-  for (var i = 0; i < total; i++){
+  for (var i = 0; i < total; i++) {
     result = result + '<p>' + item + '</p>';
   }
-return document.write(result);
+  return document.write(result);
 
 }
